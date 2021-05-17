7fc87b4da9f91e51765fd4927884e72ac9abdb1f
mengxin01-bt <mengxin01@btime.com>
2021-05-17 18:31:06 +0800

    ItemTouchHelper实现RecyclerView 上下拖动滑动删除

---
+++ Recyclerview_ItemTouchHelper/src/main/java/com/demo/recyclerview_itemtouchhelper/StartDragListener.java
@@ -0,0 +1,12 @@
+package com.demo.recyclerview_itemtouchhelper;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface StartDragListener {
+	/**
+	 * 该接口用于需要主动回调拖拽效果的
+	 * @param viewHolder
+	 */
+	public void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
