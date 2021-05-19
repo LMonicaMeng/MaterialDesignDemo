077dec48c098dee86235a6c57afbf9da9f14741e
mengxin01-bt <mengxin01@btime.com>
2021-05-19 19:21:06 +0800

    CardView

---
+++ cardview/src/main/java/com/demo/cardview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.demo.cardview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.lsn14_materialdesign_cardview.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
