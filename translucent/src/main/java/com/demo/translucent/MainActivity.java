7cf254cf66e7b99d0dd32cba7b79ff6b0db17d6f
mengxin01-bt <mengxin01@btime.com>
2021-05-19 16:51:19 +0800

    获取状态栏高度

--- translucent/src/main/java/com/demo/translucent/MainActivity.java
+++ translucent/src/main/java/com/demo/translucent/MainActivity.java
@@ -1,17 +1,20 @@
 package com.demo.translucent;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.ricky.materialdesign.translucent.R;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Toolbar toolbar;
 
 	@SuppressLint("NewApi")
 	@Override
@@ -23,8 +26,41 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		//5.0+可以直接用API来修改状态栏的颜色。
 //		getWindow().setStatusBarColor(getResources().getColor(R.color.material_blue_grey_800));
+		//1.先设置toolbar的高度
+		toolbar = (Toolbar)findViewById(R.id.toolbar);
+//		LayoutParams params = toolbar.getLayoutParams();
+//		int statusBarHeight = getStatusBarHeight(this);
+//		params.height += statusBarHeight ;
+//		toolbar.setLayoutParams(params );
+		//2.设置paddingTop，以达到状态栏不遮挡toolbar的内容。
+		toolbar.setPadding(
+				toolbar.getPaddingLeft(),
+				toolbar.getPaddingTop()+getStatusBarHeight(this),
+				toolbar.getPaddingRight(),
+				toolbar.getPaddingBottom());
 
 
+	}
+
+	/**
+	 * 获取状态栏的高度
+	 * @param context
+	 * @return
+	 */
+	private int getStatusBarHeight(Context context) {
+		// 反射手机运行的类：android.R.dimen.status_bar_height.
+		int statusHeight = -1;
+		try {
+			Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+			Object object = clazz.newInstance();
+			String heightStr = clazz.getField("status_bar_height").get(object).toString();
+			int height = Integer.parseInt(heightStr);
+			//dp--->px
+			statusHeight = context.getResources().getDimensionPixelSize(height);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return statusHeight;
 	}
 
 	@Override
