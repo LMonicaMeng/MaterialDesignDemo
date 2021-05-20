c3035e775e8106d1ae2ff591f687553841a9057e
mengxin01-bt <mengxin01@btime.com>
2021-05-20 15:02:49 +0800

    Percent Layout

---
+++ percentsupportlayout/src/main/java/com/demo/percentsupportlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.demo.percentsupportlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ricky.android.percent.support.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
