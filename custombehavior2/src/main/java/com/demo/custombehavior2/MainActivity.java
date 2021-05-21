4f74e8546ba1b02ac3205d1140bfd351c67726b4
mengxin01-bt <mengxin01@btime.com>
2021-05-21 10:45:21 +0800

    SyncScrollBehavior

---
+++ custombehavior2/src/main/java/com/demo/custombehavior2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.demo.custombehavior2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ricky.materialdesign.custombehavior.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	private TextView tv1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+}
