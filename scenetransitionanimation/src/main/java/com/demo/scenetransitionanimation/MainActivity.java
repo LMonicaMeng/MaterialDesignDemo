18e2c70dea4071a7a538782f3a1bb951be6666d8
mengxin01-bt <mengxin01@btime.com>
2021-05-24 16:53:23 +0800

    Material实现Activity转场动画

---
+++ scenetransitionanimation/src/main/java/com/demo/scenetransitionanimation/MainActivity.java
@@ -0,0 +1,59 @@
+package com.demo.scenetransitionanimation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
+
+import com.ricky.scenetransitionanimation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	private ImageView iv1;
+	private Button bt;
+	private Toolbar toolbar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		//设置允许使用转场动画
+		getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		toolbar = (Toolbar)findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		iv1 = (ImageView)findViewById(R.id.iv1);
+		bt = (Button)findViewById(R.id.bt);
+	}
+
+	public void jump(View v){
+//		startActivity(new Intent(this, SecondActivity.class));
+//		overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+
+//		ActivityOptions
+//		ActivityOptionsCompat optionsCompat = ActivityOptionsCompat
+//				.makeSceneTransitionAnimation(
+//						activity, //当前的Activity
+//						sharedElement,//共享元素---哪一个View
+//						sharedElementName)//共享元素的名称 android:transitionName="iv_meinv3"
+//		ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, iv1, "iv_meinv3");
+//		Intent intent = new Intent(this, SecondActivity.class);
+//		startActivity(intent, optionsCompat.toBundle());
+
+		//如何处理多个共享元素呢？
+//		new Pair<>(first, second)
+		ActivityOptionsCompat optionsCompat = ActivityOptionsCompat
+				.makeSceneTransitionAnimation(this, Pair.create((View)iv1, "iv1"),Pair.create((View)bt, "bt"));
+		Intent intent = new Intent(this, SecondActivity.class);
+		startActivity(intent, optionsCompat.toBundle());
+
+	}
+	
+}
